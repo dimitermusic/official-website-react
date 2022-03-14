d2dfddf2b183857fbf2d3330ee156b35fda3b8af
dimitermusic <dimiteryyordanov@gmail.com>
2022-03-14 01:08:36 -0700

    style updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,19 +12,19 @@ export default function Footer() {
             <div className='nav-icons'>
                 <a target='_blank' rel='noreferrer'
                     href='https://open.spotify.com/artist/6QfYumUXMvevU1TR729Jrr?si=hddGb2pdSfqz42rIsveMmg'>
-                    <FaSpotify className='bottom-icon' /></a>
+                    <FaSpotify className='light-icon' /></a>
                 <a target='_blank' rel='noreferrer' href='https://itunes.apple.com/us/artist/dimiter-yordanov/1212694927'>
-                    <FaApple className='bottom-icon' /></a>
+                    <FaApple className='light-icon' /></a>
                 <a target='_blank' rel='noreferrer' href='https://www.youtube.com/channel/UCfu37nGmXSfgaMsUS5yO85g'>
-                    <FaYoutube className='bottom-icon' /></a>
+                    <FaYoutube className='light-icon' /></a>
                 <a target='_blank' rel='noreferrer' href='http://instagram.com/dimitermusic'>
-                    <FaInstagram className='bottom-icon' /></a>
+                    <FaInstagram className='light-icon' /></a>
                 <a target='_blank' rel='noreferrer' href='https://www.facebook.com/dimiteryordanovmusic'>
-                    <FaFacebook className='bottom-icon' /></a>
+                    <FaFacebook className='light-icon' /></a>
                 <a target='_blank' rel='noreferrer' href='http://www.twitter.com/dimitermusic'>
-                    <FaTwitter className='bottom-icon' /></a>
+                    <FaTwitter className='light-icon' /></a>
                 <a target='_blank' rel='noreferrer' href='https://www.imdb.com/name/nm1812085/'>
-                    <FaImdb className='bottom-icon' /></a>
+                    <FaImdb className='light-icon' /></a>
             </div>
             <p id='copyright-text'>© {thisYear} Dimiter Yordanov. All Rights Reserved. Powered by <a className='github-link' href={gitHubLink} target='_blank' rel='noreferrer'>Dimiter Yordanov</a>.</p>
         </>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import '../styles/style.css'
 import { FaSpotify, FaApple, FaYoutube, FaInstagram, FaFacebook, FaTwitter, FaImdb } from "react-icons/fa";
+import lieToMeText from '../images/lie-to-me-text.png'
 import lieToMe from '../images/lie-to-me.webp'
 import spotify from '../images/Y-Spotify.png'
 import apple from '../images/Y-AppleMusic.png'
@@ -14,19 +15,19 @@ export default function Landing() {
                 <div className='nav-icons'>
                     <a target='_blank' rel='noreferrer'
                         href='https://open.spotify.com/artist/6QfYumUXMvevU1TR729Jrr?si=hddGb2pdSfqz42rIsveMmg'>
-                        <FaSpotify className='icon' /></a>
+                        <FaSpotify className='dark-icon' /></a>
                     <a target='_blank' rel='noreferrer' href='https://itunes.apple.com/us/artist/dimiter-yordanov/1212694927'>
-                        <FaApple className='icon' /></a>
+                        <FaApple className='dark-icon' /></a>
                     <a target='_blank' rel='noreferrer' href='https://www.youtube.com/channel/UCfu37nGmXSfgaMsUS5yO85g'>
-                        <FaYoutube className='icon' /></a>
+                        <FaYoutube className='dark-icon' /></a>
                     <a target='_blank' rel='noreferrer' href='http://instagram.com/dimitermusic'>
-                        <FaInstagram className='icon' /></a>
+                        <FaInstagram className='dark-icon' /></a>
                     <a target='_blank' rel='noreferrer' href='https://www.facebook.com/dimiteryordanovmusic'>
-                        <FaFacebook className='icon' /></a>
+                        <FaFacebook className='dark-icon' /></a>
                     <a target='_blank' rel='noreferrer' href='http://www.twitter.com/dimitermusic'>
-                        <FaTwitter className='icon' /></a>
+                        <FaTwitter className='dark-icon' /></a>
                     <a target='_blank' rel='noreferrer' href='https://www.imdb.com/name/nm1812085/'>
-                        <FaImdb className='icon' /></a>
+                        <FaImdb className='dark-icon' /></a>
                 </div>
                 <h1>Dimiter Yordanov</h1>
             </nav>
@@ -35,12 +36,13 @@ export default function Landing() {
                     <div className='column'>
                         <a target='_blank' rel='noreferrer'
                             href='https://open.spotify.com/album/2tqOIJ7JQGANnCAE25bsyx?si=2S13s8dgTL-IUj2e9A7jJQ'>
-                            {<img id='album-cover' src={lieToMe}
-                                alt='dimiter lie to me trees lake fence sky' />}
+                            <img id='album-cover' src={lieToMe}
+                                alt='dimiter lie to me trees lake fence sky' />
                         </a>
                     </div>
                     <div className='column'>
-                        <p id='release-text'><span id='bold'>LIE TO ME</span> EP OUT NOW</p>
+                        <img id='lie-to-me-text' src={lieToMeText} alt='dimiter lie to me text' />
+                        <p id='release-text'>OUT NOW</p>
                         <div className='release-btn-group'>
                             <a target='_blank' rel='noreferrer'
                                 href='https://open.spotify.com/album/2tqOIJ7JQGANnCAE25bsyx?si=2S13s8dgTL-IUj2e9A7jJQ'>
