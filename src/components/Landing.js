0db5a1e2f42b66b2ae49aa24282409e062056f77
Dimiter Yordanov <dimiteryyordanov@gmail.com>
2022-07-11 00:11:18 -0700

    removing alt properties from images as they are overriding meta description in Google results

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,53 +8,50 @@ import amazon from '../images/amazon-music-logo.png';
 import pandora from '../images/pandora.png';
 
 export default function Landing() {
+  // Function that handles dynamically rendering release buttons if album cover has loaded.
+  function handleImageLoaded() {
+    document.getElementById('col-2').style.display = 'flex';
+    return;
+  }
 
-    // Function that handles dynamically rendering release buttons if album cover has loaded.
-    function handleImageLoaded() {
-        document.getElementById('col-2').style.display = 'flex';
-        return;
-    }
-
-    return (
-        <div id='landing'>
-            <div id='release-section'>
-                <div className='column'>
-                    <img onLoad={handleImageLoaded} id='album-cover' src={lieToMe}
-                        alt='dimiter lie to me trees lake fence sky' />
-                </div>
-                <div className='column' id='col-2'>
-                    <img id='lie-to-me-text' src={lieToMeText} alt='dimiter lie to me text' />
-                    <p id='release-text'>OUT NOW</p>
-                    <div className='release-btn-group'>
-                        <a href='https://open.spotify.com/album/2tqOIJ7JQGANnCAE25bsyx?si=2S13s8dgTL-IUj2e9A7jJQ'>
-                            <div className='release-btn'>
-                                <img className='release-logo' src={spotify} alt='spotify music' />
-                            </div>
-                        </a>
-                        <a href='https://music.apple.com/us/album/lie-to-me-ep/1603910239'>
-                            <div className='release-btn'>
-                                <img className='release-logo' src={apple} alt='apple music' />
-                            </div>
-                        </a>
-                        <a href='https://music.apple.com/us/album/lie-to-me-ep/1603910239?ign-itscg=30440&ign-itsct=catchall_p2'>
-                            <div className='release-btn'>
-                                <img className='release-logo' src={itunes} alt='itunes music' />
-                            </div>
-                        </a>
-                        <a href='https://music.amazon.com/albums/B09Q112YV6'>
-                            <div className='release-btn'>
-                                <img className='release-logo' src={amazon}
-                                    alt='amazon music' />
-                            </div>
-                        </a>
-                        <a href='https://www.pandora.com/artist/dimiter-yordanov/lie-to-me-single/ALh7qfx2gl7ch3c'>
-                            <div className='release-btn'>
-                                <img className='release-logo' src={pandora} alt='pandora music' />
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id='landing'>
+      <div id='release-section'>
+        <div className='column'>
+          <img onLoad={handleImageLoaded} id='album-cover' src={lieToMe} />
         </div>
-    );
+        <div className='column' id='col-2'>
+          <img id='lie-to-me-text' src={lieToMeText} />
+          <p id='release-text'>OUT NOW</p>
+          <div className='release-btn-group'>
+            <a href='https://open.spotify.com/album/2tqOIJ7JQGANnCAE25bsyx?si=2S13s8dgTL-IUj2e9A7jJQ'>
+              <div className='release-btn'>
+                <img className='release-logo' src={spotify} />
+              </div>
+            </a>
+            <a href='https://music.apple.com/us/album/lie-to-me-ep/1603910239'>
+              <div className='release-btn'>
+                <img className='release-logo' src={apple} />
+              </div>
+            </a>
+            <a href='https://music.apple.com/us/album/lie-to-me-ep/1603910239?ign-itscg=30440&ign-itsct=catchall_p2'>
+              <div className='release-btn'>
+                <img className='release-logo' src={itunes} />
+              </div>
+            </a>
+            <a href='https://music.amazon.com/albums/B09Q112YV6'>
+              <div className='release-btn'>
+                <img className='release-logo' src={amazon} />
+              </div>
+            </a>
+            <a href='https://www.pandora.com/artist/dimiter-yordanov/lie-to-me-single/ALh7qfx2gl7ch3c'>
+              <div className='release-btn'>
+                <img className='release-logo' src={pandora} />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
