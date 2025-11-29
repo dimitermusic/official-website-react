c3eda0d227439fc8221edcebf8fb8ccbfe1ff3e7
Dimiter Yordanov <dimitermusic@gmail.com>
2025-11-28 16:34:06 -0800

    remove unused var

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ import Icons from "./Icons";
 export default function Footer() {
   // Get and dynamically render current year
   let thisYear = new Date().toDateString().slice(11);
-  let gitHubLink = "https://www.github.com/dimitermusic";
 
   return (
     <footer id="footer" className="awal-credits-section">
