afad5460480379943b656ef3347e1927f63f84b7
Dimiter Yordanov <dimitermusic@gmail.com>
2025-11-28 16:48:02 -0800

    improved cicd

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders all main sections by ID", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const ids = ["film-tv", "watch", "tour", "listen", "subscribe"];
+
+  ids.forEach((id) => {
+    expect(document.getElementById(id)).toBeInTheDocument();
+  });
 });
